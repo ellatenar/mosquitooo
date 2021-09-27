25e086220b1fe07b98c2f3a920889c6b8b3bfb10
Ella Heron <heronsound95@gmail.com>
2021-09-27 18:43:50 -0400

    add 1 article to archive

--- src/pages/archive/index.js
+++ src/pages/archive/index.js
@@ -12,22 +12,23 @@ const Archive = () => {
             <Link to="/archive/reading">Reading, 2017 – 2020</Link>
           </p>
           <p><Link to="/archive/toward-the-jubilee">Toward the Jubilee</Link></p>
-          <p><Link to="">Mourning, Militancy, and Messiness</Link></p>
-          <p><Link to="">Psychic Endlessness</Link></p>
-          <p><Link to="">Vacant Presence</Link></p>
-          <p><Link to="">Improvisational Readership</Link></p>
-          <p><Link to="">Queer Charisma</Link></p>
-          <p><Link to="">Who Owns Public Space?</Link></p>
-          <p><Link to="">On Mundanity</Link></p>
-          <p><Link to="">Loneliness Studies</Link></p>
-          <p><Link to="">An Aesthetic Headache</Link></p>
-          <p><Link to="">Disaster Art, Disaster Activism</Link></p>
-          <p><Link to="">Real Violence</Link></p>
-          <p><Link to="">Worst Fear, Best Fantasy</Link></p>
-          <p><Link to="">Print on Demand</Link></p>
-          <p><Link to="">Bad Apples</Link></p>
-          <p><Link to="">Socialism, Barbarism, and Fatigue</Link></p>
-          <p><Link to="/archive/channelizer">Channelizer</Link></p>
+          <p>More to come...</p>
+          {/* <p><Link to="/archive/mourning-militancy">Mourning, Militancy, and Messiness</Link></p>
+          <p><Link to="/archive/psychic-endlessness">Psychic Endlessness</Link></p>
+          <p><Link to="/archive/vacant-presence">Vacant Presence</Link></p>
+          <p><Link to="/archive/improv-readership">Improvisational Readership</Link></p>
+          <p><Link to="/archive/queer-charismas">Queer Charisma</Link></p>
+          <p><Link to="/archive/who-owns-public-space">Who Owns Public Space?</Link></p>
+          <p><Link to="/archive/on-mundanity">On Mundanity</Link></p>
+          <p><Link to="/archive/loneliness-studies">Loneliness Studies</Link></p>
+          <p><Link to="/archive/an-aesthetic-headache">An Aesthetic Headache</Link></p>
+          <p><Link to="/archive/disaster-art">Disaster Art, Disaster Activism</Link></p>
+          <p><Link to="/archive/real-violence">Real Violence</Link></p>
+          <p><Link to="/archive/worst-fear">Worst Fear, Best Fantasy</Link></p>
+          <p><Link to="/archive/print-on-demand">Print on Demand</Link></p>
+          <p><Link to="/archive/bad-apples">Bad Apples</Link></p>
+          <p><Link to="/archive/socialism-barbarism">Socialism, Barbarism, and Fatigue</Link></p>
+          <p><Link to="/archive/channelizer">Channelizer</Link></p> */}
       </Layout>
     )
 }
