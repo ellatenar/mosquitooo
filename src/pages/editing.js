85ada88b7e3ca197a88c6dab27d669dad7ad209a
Ella Heron <heronsound95@gmail.com>
2022-02-11 08:38:08 -0500

    fix typo

--- src/pages/editing.js
+++ src/pages/editing.js
@@ -5,28 +5,51 @@ const Editing = () => {
   return (
     <Layout pageName="Mosquito" title="Editing">
       <p className="text">
-        I’m available for editing work and happy to work on projects of all shapes and sizes, from short
-        essays to booklength manuscripts. I’ve worked as an editor for the better part of the past decade,
-        including in full-time positions at ICA Philadelphia, College Art Association, and Freeman’s.
-        Recent projects I’m grateful to have worked on include Stephen Housewright’s <i>Partners</i> and a
-        collection of essays about Jerry Hunt, <i>Transmissions from the Pleroma</i> (both Blank Forms,
-        2022), a gallery reader for Niki de Saint Phalle’s <i>Joy Revolution</i> at Salon 94 Gallery, and an
-        exhibition catalogue for Jammie Holmes’s <i>Pieces of a Ma</i>n at Library Street Collective.
+        I’m available for editing work and happy to work on projects of all
+        shapes and sizes, from short essays to booklength manuscripts. I’ve
+        worked as an editor for the better part of the past decade, including in
+        full-time positions at ICA Philadelphia, College Art Association, and
+        Freeman’s. Recent projects I’m grateful to have worked on include
+        Stephen Housewright’s <i>Partners</i> and a collection of essays about
+        Jerry Hunt, <i>Transmissions from the Pleroma</i> (both Blank Forms,
+        2022), a gallery reader for Niki de Saint Phalle’s <i>Joy Revolution</i>{" "}
+        at Salon 94 Gallery, and an exhibition catalogue for Holmes’s{" "}
+        <i>Pieces of a Man</i> at Library Street Collective.
       </p>
-      <p>
-        Past copyediting projects:
-      </p>
-        <li><i>Partners</i> (New York: Blank Forms, 2022).</li>
-        <li><i>Transmissions from the Pleroma</i> (New York: Blank Forms, 2022).</li>
-        <li><i>Jammie Holmes: Pieces of a Man</i> (Detroit: Library Street Collective, 2021).</li>
-        <li><i>Sam Friedman</i> (Detroit: Library Street Collective, 2021).</li>
-        <li><i>An Index to Art and Politics: 25 Years of Vera List Center Fellowships</i> (New York: Vera
-        List Center for Art and Politics, 2018).</li>
-        <li><i>Myths of the Marble</i> (Philadelphia: ICA, 2018).</li>
-        <li><i>Speech/Acts</i> (Philadelphia: ICA, 2018).</li>
-        <li><i>the moon will sink into the street</i> (Philadelphia: ICA, 2018) (commissioning editor).</li>
-        <li><i>Fire Bill’s Spook Kit</i> (Ieper, BE: In Flanders Fields Museum, 2017).</li>
-
+      <p>Past copyediting projects:</p>
+      <li>
+        <i>Partners</i> (New York: Blank Forms, 2022).
+      </li>
+      <li>
+        <i>Transmissions from the Pleroma</i> (New York: Blank Forms, 2022).
+      </li>
+      <li>
+        <i>Jammie Holmes: Pieces of a Man</i> (Detroit: Library Street
+        Collective, 2021).
+      </li>
+      <li>
+        <i>Sam Friedman</i> (Detroit: Library Street Collective, 2021).
+      </li>
+      <li>
+        <i>
+          An Index to Art and Politics: 25 Years of Vera List Center Fellowships
+        </i>{" "}
+        (New York: Vera List Center for Art and Politics, 2018).
+      </li>
+      <li>
+        <i>Myths of the Marble</i> (Philadelphia: ICA, 2018).
+      </li>
+      <li>
+        <i>Speech/Acts</i> (Philadelphia: ICA, 2018).
+      </li>
+      <li>
+        <i>the moon will sink into the street</i> (Philadelphia: ICA, 2018)
+        (commissioning editor).
+      </li>
+      <li>
+        <i>Fire Bill’s Spook Kit</i> (Ieper, BE: In Flanders Fields Museum,
+        2017).
+      </li>
     </Layout>
   );
 };
