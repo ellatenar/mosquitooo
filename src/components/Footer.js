a9f43f50001e19ca94bbc769da6d976f4783a09f
Ella Heron <heronsound95@gmail.com>
2022-06-07 21:30:24 -0400

    update ella site link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+const Link0 = () => <a href="https://heron.zone/">ella heron</a>;
 const Link1 = () => <a href="https://www.sunyoungoh.com/">Sun Young Oh</a>;
 const Link2 = () => (
   <a href="https://authentic.website/">Christina Janus & Desmond Wong</a>
@@ -8,7 +9,7 @@ const Link2 = () => (
 const Footer = () => {
   return (
     <footer>
-      site by ella heron. fonts by {<Link1 />} and {<Link2 />}.
+      site by {<Link0 />}. fonts by {<Link1 />} and {<Link2 />}.
     </footer>
   );
 };
