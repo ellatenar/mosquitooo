c3fe30c15b5f4d02982e6c2eabd5534b5c8d85c0
Ella Heron <heronsound95@gmail.com>
2024-06-07 12:40:22 -0400

    update documentation

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 [![Netlify Status](https://api.netlify.com/api/v1/badges/582049bf-c173-4d1f-a566-cd8189baa509/deploy-status)](https://app.netlify.com/sites/nervous-goodall-2a57ec/deploys)
 
 A Gatsby site for Heather Holmes, made in 2022 with love
+
+# Changelog
+
+- **6/7/2024**: nightly netlify builds via serverless function/build hooks
--- netlify/functions/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy.js
@@ -4,8 +4,7 @@ import { schedule } from "@netlify/functions"
 const BUILD_HOOK =
   "https://api.netlify.com/build_hooks/66633127d83d964dccbd04fe"
 
-// Schedules the handler function to run at midnight on
-// Mondays, Wednesday, and Friday
+// Schedules the handler function to run 00:05 nightly
 const handler = schedule("5 0 * * *", async () => {
   await fetch(BUILD_HOOK, {
     method: "POST",
